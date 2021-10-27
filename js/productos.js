aa703251b9499a641e6470c7ff7266d7b8147153
Matias <villanuevamatias2001@gmail.com>
2021-10-27 19:18:13 -0300

    popUp mejorado

--- js/productos.js
+++ js/productos.js
@@ -1,3 +1,7 @@
-function popUp() {
+setTimeout(function popUp() {
   document.getElementById("wrapperModal").style.display = "flex";
+}, 1000);
+
+function closePopUp() {
+  document.getElementById("wrapperModal").style.display = "none";
 }
