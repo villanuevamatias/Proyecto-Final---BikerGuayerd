4bda094d882e6aa9e13eca2e1f9cf7ac1bd38c92
Axel Solari-Pc <axelsolari@live.com.ar>
2021-10-26 21:59:05 -0300

    agregado js login

--- js/home.js
+++ js/home.js
@@ -26,3 +26,29 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+
+//---LOGIN---
+
+function store(){
+  /*Recibe los datos del imput*/
+  var inputUser = document.getElementById("us");
+  var inputEmail = document.getElementById("em");
+  /*Almacena los datos en el LocalStorage*/
+  localStorage.setItem("usuarios", inputUser.value);
+  localStorage.setItem("Email", inputEmail.value);
+  // console.log(inputUser.value);
+  // console.log(inputEmail.value);
+  /*Limpia los campos de los imputs*/
+  document.getElementById("us").value="";
+  document.getElementById("em").value="";
+}
+
+function validation(){
+  var nombre = localStorage.getItem("Usuario");
+  var correo = localStorage.getItem("Email");
+  console.log(nombre);
+  console.log(correo);
+
+}
+validation();
