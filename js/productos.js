2e46c02c1b63588f41810a36a85bf0a8d2210d29
Matias <villanuevamatias2001@gmail.com>
2021-10-28 20:42:03 -0300

    ultimas modificaciones popup HOTSALE

--- js/productos.js
+++ js/productos.js
@@ -1,113 +1,110 @@
+const modalContainerEfecto = document.querySelector(".modalContainer");
 
 setTimeout(function popUp() {
-  let Novedades=sessionStorage.getItem("Novedades")
-  console.log(Novedades+"p");
-  if(Novedades){ // solo saldra si el usuario quiere recibir novedades.
+  let Novedades = sessionStorage.getItem("Novedades");
+  console.log(Novedades + "p");
+  if (Novedades) {
+    // solo saldra si el usuario quiere recibir novedades.
     console.log("popup");
-  document.getElementById("wrapperModal").style.display = "flex";
+    document.getElementById("wrapperModal").style.visibility = "visible";
+    modalContainerEfecto.classList.add("active");
   }
-}, 3000);
+}, 1000);
 
 function closePopUp() {
   document.getElementById("wrapperModal").style.display = "none";
 }
 
-
-
 function ocultarLogin() {
-  console.log("BORRAR")
-  var opcion= sessionStorage.getItem("ocultarLogin");
+  console.log("BORRAR");
+  var opcion = sessionStorage.getItem("ocultarLogin");
   console.log(opcion);
-  if(opcion==="si"){
+  if (opcion === "si") {
     document.getElementById("contenedorLogin").style.display = "none";
   }
-  
 }
- setTimeout( function ocultarLogin() {
-  console.log("BORRAR")
-  var opcion= sessionStorage.getItem("ocultarLogin");
+setTimeout(function ocultarLogin() {
+  console.log("BORRAR");
+  var opcion = sessionStorage.getItem("ocultarLogin");
   console.log(opcion);
-  if(opcion==="si"){
+  if (opcion === "si") {
     document.getElementById("contenedorLogin").style.display = "none";
   }
-  
 }, 0);
 //---LOGIN---//
 /*SALUDO*/
-const saludoUs = document.querySelector('#saludoUser');
+const saludoUs = document.querySelector("#saludoUser");
 const today = new Date();
 var h = today.getHours();
 var m = today.getMinutes();
 
-function saludar(){
-  if (m<10) {
+function saludar() {
+  if (m < 10) {
     m = "0" + m;
   }
 
   saludoUs.style.visibility = "visible";
-  userS = localStorage.getItem("usuarios"); 
-  document.getElementById("timeH").innerHTML = h + ":" + m + " " + "Hs" + " " + "-";
+  userS = localStorage.getItem("usuarios");
+  document.getElementById("timeH").innerHTML =
+    h + ":" + m + " " + "Hs" + " " + "-";
   document.getElementById("userName").innerHTML = userS;
 }
-var userS
+var userS;
 /* SALUDO*/
 
-function store(){
+function store() {
   /*Recibe los datos del imput*/
   var inputUser = document.getElementById("us").value;
   var inputEmail = document.getElementById("em").value;
-  var Novedades =document.getElementById("Novedades");
+  var Novedades = document.getElementById("Novedades");
   // Comprobar si local esta guardado
   var correo = localStorage.getItem("Email");
   console.log(Novedades.checked);
-if(inputEmail===correo){
-  //saludar(); //si existe el usuario saludar
-  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
-  ocultarLogin();
-  if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
-   sessionStorage.setItem("Novedades", "si");
-     mostrarhotsale();
+  if (inputEmail === correo) {
+    //saludar(); //si existe el usuario saludar
+    sessionStorage.setItem("ocultarLogin", "si"); // esta variable temporaria nos servira para mostrar o los login de los demas site.
+    ocultarLogin();
+    if (Novedades.checked) {
+      //si el check box fue tildado mostraremos el logo flotante de hotsale.
+      sessionStorage.setItem("Novedades", "si");
+      mostrarhotsale();
+    }
+  } else {
+    // en  caso que el usuario no este guardado en local storage
+    //asignarcupon ();// funcion que asignara cupon y desprendera un popup
+    if (Novedades.checked) {
+      //si el check box fue tildado mostraremos el logo flotante de hotsale.
+      mostrarhotsale();
+      sessionStorage.setItem("Novedades", "si");
+    }
+    sessionStorage.setItem("ocultarLogin", "si"); // esta variable temporaria nos servira para mostrar o los login de los demas site.
+    ocultarLogin();
 
+    /*Almacena los datos en el LocalStorage*/
+    localStorage.setItem("usuarios", inputUser);
+    localStorage.setItem("Email", inputEmail);
+
+    // console.log(inputUser.value);
+    // console.log(inputEmail.value);
+    /*Limpia los campos de los imputs*/
+
+    // document.getElementById("us").value="";
+    // document.getElementById("em").value="";
   }
-
-}else{ // en  caso que el usuario no este guardado en local storage 
- //asignarcupon ();// funcion que asignara cupon y desprendera un popup
- if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
-     mostrarhotsale();
-  sessionStorage.setItem("Novedades", "si");
-
-  }
-  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
-  ocultarLogin();
-
-  /*Almacena los datos en el LocalStorage*/
-  localStorage.setItem("usuarios", inputUser);
-  localStorage.setItem("Email", inputEmail);
-
-
-  // console.log(inputUser.value);
-  // console.log(inputEmail.value);
-  /*Limpia los campos de los imputs*/
-
-  // document.getElementById("us").value="";
-  // document.getElementById("em").value="";
 }
-}
-function mostrarhotsale(){ 
-  document.getElementById("logohotsale").style.display="flex";
+function mostrarhotsale() {
+  document.getElementById("logohotsale").style.display = "flex";
 }
 function ocultarhotsale() {
-  document.getElementById("logohotsale").style.display="none";
-
+  document.getElementById("logohotsale").style.display = "none";
 }
 
 ocultarLogin();
 function ocultarLogin() {
-  var opcion= sessionStorage.getItem("ocultarLogin");
+  var opcion = sessionStorage.getItem("ocultarLogin");
   console.log(opcion);
-  if(opcion==="si"){
-    saludar()
-    document.getElementById("contenedorLogin").style.display="none";
+  if (opcion === "si") {
+    saludar();
+    document.getElementById("contenedorLogin").style.display = "none";
   }
-  
 }
