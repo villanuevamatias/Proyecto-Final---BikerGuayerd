cb3b0e29b1a49a6317802819421dd560c06348a7
darioboroski <darioboroski@gmail.com>
2021-10-28 04:32:52 -0300

    llevar el logueo y el hotsale a todas las pag, validacion de novedades, y display none en simult.

--- js/home.js
+++ js/home.js
@@ -27,61 +27,96 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-
 //---LOGIN---//
 
 function store(){
   /*Recibe los datos del imput*/
-  var inputUser = document.getElementById("us");
-  var inputEmail = document.getElementById("em");
+  var inputUser = document.getElementById("us").value;
+  var inputEmail = document.getElementById("em").value;
+  var Novedades =document.getElementById("Novedades");
+  // Comprobar si local esta guardado
+  var correo = localStorage.getItem("Email");
+  console.log(Novedades.checked);
+if(inputEmail===correo){
+  //saludar(); //si existe el usuario saludar
+  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
+  ocultarLogin();
+  if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
+   sessionStorage.setItem("Novedades", "si");
+     mostrarhotsale();
+
+  }
+
+}else{ // en  caso que el usuario no este guardado en local storage 
+ //asignarcupon ();// funcion que asignara cupon y desprendera un popup
+ if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
+     mostrarhotsale();
+  sessionStorage.setItem("Novedades", "si");
+
+  }
+  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
+  ocultarLogin();
+
   /*Almacena los datos en el LocalStorage*/
-  localStorage.setItem("usuarios", inputUser.value);
-  localStorage.setItem("Email", inputEmail.value);
+  localStorage.setItem("usuarios", inputUser);
+  localStorage.setItem("Email", inputEmail);
+
+
   // console.log(inputUser.value);
   // console.log(inputEmail.value);
   /*Limpia los campos de los imputs*/
-  document.getElementById("us").value="";
-  document.getElementById("em").value="";
+
+  // document.getElementById("us").value="";
+  // document.getElementById("em").value="";
+}
+}
+function mostrarhotsale(){ 
+  document.getElementById("logohotsale").style.display="flex";
+}
+function ocultarhotsale() {
+  document.getElementById("logohotsale").style.display="none";
+
 }
 
-function validation(){
-  var nombre = localStorage.getItem("Usuario");
-  var correo = localStorage.getItem("Email");
-  console.log(nombre);
-  console.log(correo);
-
+ocultarLogin();
+function ocultarLogin() {
+  var opcion= sessionStorage.getItem("ocultarLogin");
+  console.log(opcion);
+  if(opcion==="si"){
+    document.getElementById("contenedorLogin").style.display="none";
+  }
+  
 }
-validation();
 
 //--FIN-LOGIN-//
 
 //--CODE POPUP--//
 
-const popupMain = document.querySelector('.popup');
-const popupFondo = document.querySelector('.popup-fondo');
-const close = document.querySelector('.popup-close');
-const close2 = document.querySelector('#no');
+// const popupMain = document.querySelector('.popup');
+// const popupFondo = document.querySelector('.popup-fondo');
+// const close = document.querySelector('.popup-close');
+// const close2 = document.querySelector('#no');
 
 
 
-function popUp (){
-    popupFondo.style.visibility = 'visible';
-    popupMain.classList.add('active');
-}
+// function popUp (){
+//     popupFondo.style.visibility = 'visible';
+//     popupMain.classList.add('active');
+// }
 
-function onView(){
-    time = setTimeout(popUp, 2000);
-}
-onView();
+// function onView(){
+//     time = setTimeout(popUp, 2000);
+// }
+// onView();
 
-close.addEventListener('click', () => {
-    popupFondo.style.display = 'none';
-    popupMain.classList.remove('active');
-});
+// close.addEventListener('click', () => {
+//     popupFondo.style.display = 'none';
+//     popupMain.classList.remove('active');
+// });
 
-close2.addEventListener('click', () => {
-    popupFondo.style.display = 'none';
-    popupMain.classList.remove('active');
-});
+// close2.addEventListener('click', () => {
+//     popupFondo.style.display = 'none';
+//     popupMain.classList.remove('active');
+// });
 
-//FIN CODE POPUP//
+//FIN CODE POPUP
--- js/productos.js
+++ js/productos.js
@@ -1,8 +1,84 @@
+
 setTimeout(function popUp() {
+  let Novedades=sessionStorage.getItem("Novedades")
+  console.log(Novedades+"p");
+  if(Novedades){ // solo saldra si el usuario quiere recibir novedades.
+    console.log("popup");
   document.getElementById("wrapperModal").style.display = "flex";
+  }
 }, 1000);
 
 function closePopUp() {
   document.getElementById("wrapperModal").style.display = "none";
 }
 
+
+
+function ocultarLogin() {
+  console.log("BORRAR")
+  var opcion= sessionStorage.getItem("ocultarLogin");
+  console.log(opcion);
+  if(opcion==="si"){
+    document.getElementById("contenedorLogin").style.display = "none";
+  }
+  
+}
+ setTimeout( function ocultarLogin() {
+  console.log("BORRAR")
+  var opcion= sessionStorage.getItem("ocultarLogin");
+  console.log(opcion);
+  if(opcion==="si"){
+    document.getElementById("contenedorLogin").style.display = "none";
+  }
+  
+}, 1000);
+//---LOGIN---//
+
+function store(){
+  /*Recibe los datos del imput*/
+  var inputUser = document.getElementById("us").value;
+  var inputEmail = document.getElementById("em").value;
+  var Novedades =document.getElementById("Novedades");
+  // Comprobar si local esta guardado
+  var correo = localStorage.getItem("Email");
+  console.log(Novedades.checked);
+if(inputEmail===correo){
+  //saludar(); //si existe el usuario saludar
+  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
+  ocultarLogin();
+  if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
+   sessionStorage.setItem("Novedades", "si");
+     mostrarhotsale();
+
+  }
+
+}else{ // en  caso que el usuario no este guardado en local storage 
+ //asignarcupon ();// funcion que asignara cupon y desprendera un popup
+ if(Novedades.checked){//si el check box fue tildado mostraremos el logo flotante de hotsale.
+     mostrarhotsale();
+  sessionStorage.setItem("Novedades", "si");
+
+  }
+  sessionStorage.setItem("ocultarLogin", "si" );// esta variable temporaria nos servira para mostrar o los login de los demas site.
+  ocultarLogin();
+
+  /*Almacena los datos en el LocalStorage*/
+  localStorage.setItem("usuarios", inputUser);
+  localStorage.setItem("Email", inputEmail);
+
+
+  // console.log(inputUser.value);
+  // console.log(inputEmail.value);
+  /*Limpia los campos de los imputs*/
+
+  // document.getElementById("us").value="";
+  // document.getElementById("em").value="";
+}
+}
+function mostrarhotsale(){ 
+  document.getElementById("logohotsale").style.display="flex";
+}
+function ocultarhotsale() {
+  document.getElementById("logohotsale").style.display="none";
+
+}
